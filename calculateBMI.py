3fefccdc6066583d12f604d34b73703fcf5b4f76
Izardo <isabelladoylegti@gmail.com>
2021-02-01 21:42:20 +0000

    Wrote program to calculate BMI

---
+++ calculateBMI.py
@@ -0,0 +1,9 @@
+# this program calculates BMI
+# author: Isabella Doyle
+
+weight = int(input("Enter weight(Kg):"))    # asks user to input their weight
+height = int(input("Enter height(cm):"))    # asks user to input their height
+
+BMI = str(round(weight/((height/100)**2)))  # calculates BMI
+
+print("Your BMI is " + BMI + ".")           # prints the users BMI
