063db1c0992312e7012b7a8c3c95322240c55066
Izardo <isabelladoylegti@gmail.com>
2021-02-06 19:52:38 +0000

    Added 2 decimal place rounding to line 7 sum

--- calculateBMI.py
+++ calculateBMI.py
@@ -1,9 +1,9 @@
-# this program calculates BMI
-# author: Isabella Doyle
+# This program calculates BMI
+# Author: Isabella Doyle
 
 weight = int(input("Enter weight(Kg):"))    # asks user to input their weight
 height = int(input("Enter height(cm):"))    # asks user to input their height
 
-BMI = str(round(weight/((height/100)**2)))  # calculates BMI
+BMI = str(round(weight/((height/100)**2),2))  # calculates BMI
 
 print("Your BMI is " + BMI + ".")           # prints the users BMI
