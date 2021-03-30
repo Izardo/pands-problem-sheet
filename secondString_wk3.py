713ab448a22d0e7377be9888f093bc21259e923f
Izardo <isabelladoylegti@gmail.com>
2021-03-30 13:54:00 +0100

    improved readability by adding blank space

--- secondString_wk3.py
+++ secondString_wk3.py
@@ -1,5 +1,6 @@
 # This program requests a string input from the user 
 # and outputs every second character in reverse order. 
+
 # Author: Isabella Doyle
 
 string = input("Enter text:")   # requests string input from user
