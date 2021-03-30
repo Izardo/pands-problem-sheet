3a8587fcb4a19f85273e72114b45aa6936791627
Izardo <isabelladoylegti@gmail.com>
2021-03-30 14:10:15 +0100

    edited code so as to input filename as an argument

--- es_wk7.py
+++ es_wk7.py
@@ -1,12 +1,13 @@
-# This program allows the users to to count the number of times
+# This program allows the users to count the number of times
 # the character "e" appears in a chosen file
 
 # Author: Isabella Doyle
 
-fileName = str(input("Enter the file you wish to use: "))   # requests the user to input the file name they wish to use 
+def readText(fileName):                 # creates a function with one parameter (will be used to input a file name)
 
-with open(fileName, 'rt') as f:     # opens the file in read text mode
-    readTxt = f.read()              # reads in the file and assigns it to the value readTxt
-    count = readTxt.count("e")      # count method returns no. of instances the character "e" appears in the file
-    print(count)                    # prints the count 
+    with open(fileName, 'rt') as f:     # opens the file in read-text mode and assigns it the alias "f"
+        read = f.read()                 # reads in the file and assigns it to the value read
+        count = readTxt.count("e")      # count method returns no. of instances the character "e" appears in the file
+        print(count)                    # prints the count when the function is called
 
+readText("moby-dick.txt")               # calls the readText function with the file name input as an argument
