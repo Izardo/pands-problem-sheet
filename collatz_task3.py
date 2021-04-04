3c2efdefe0a173cdfea69cd182686fd85aef5368
Izardo <isabelladoylegti@gmail.com>
2021-04-04 20:42:34 +0100

    Edited comments, added references

--- collatz_task3.py
+++ collatz_task3.py
@@ -1,15 +1,16 @@
 # This program requests a positive integer input from the user and performs 
 # various arithmetic depending on whether the number is even or odd
+
 # Author: Isabella Doyle
 
-num = int(input("Enter a positive integer: "))  # requests input of positive int from user and converts to integer
+num = int(input("Enter a positive integer: "))  # requests input of positive int from user, converts to integer and assigns to variable num
 numList = [num]                                 # assigns a list (which stores values from the sums below) to the variable numList 
 
 while num <= 0:                                 # while loop filters out negative numbers so program runs smoothly
     print("Bad input")                          # informs the user of wrong input
     num = int(input("Enter a positive integer: ")) # requests another number input and converts to integer
 
-while num != 1:                                 # while loop to repeatedly perform sum until num equals 0
+while num != 1:                                 # while loop to repeatedly perform sum until num equals 1
     if num % 2 == 0:                            # checks if num is even using the modulus operator
         num = num / 2                           # if even, this block of code is performed
         numList.append(num)                     # value produced by sum is added to numList
@@ -19,3 +20,18 @@ while num != 1:                                 # while loop to repeatedly perfo
 
 print([int(num) for num in numList])            # converts contents of numList from list objects to integer
                                                 # objects and prints the contents of the list
+
+
+'''
+REFERENCES:
+
+[1] "How to Convert a Float List to an Integer List in Python" - Evgeny Erunov <b/>
+    Available at:
+    https://blog.finxter.com/how-to-convert-a-float-list-to-an-integer-list-in-python/#:~:text=The%20most%20Pythonic%20way%20to,the%20int(x)%20constructor
+    [Accessed: 09/01/21]
+[2] "While Loops", A Whirlwind Tour of Python - Jake Vanderplas pp. 37
+[3] "Python Lists" - w3schools
+    Available at: https://www.w3schools.com/python/python_lists.asp [Accessed: 09/01/2021]
+[4] "Python For Loops" - w3schools
+    Available at: https://www.w3schools.com/python/python_for_loops.asp [Accessed: 09/01/2021]
+'''
--- secondString_task2.py
+++ secondString_task2.py
@@ -3,8 +3,23 @@
 
 # Author: Isabella Doyle
 
-string = input("Enter text:")   # requests string input from user
-modString = string[::-2]        # when printed it will output
-                                # every second character in reverse
+string = input("Enter text:")   # requests string input from user and assigns input to the variable 'string'
+modString = string[::-2]        # modifies value in 'string', starting at index 0, we are left with every 
+                                # second character in reverse and assigns it to the variable modString 
+                                # (Example: Hello -> olh)
 
+# prints the original string as well as the new modified string, 
+# the values are inserted in print statement using the .format method
 print("Your original string: {}. New modified string: {}".format(string, modString))
+
+
+'''
+REFERENCES:
+
+[1] "Built-in Functions" - The Python Standard Library 
+    Available at: https://docs.python.org/3/library/functions.html [Accessed: 29/01/21]
+[2] "Python Numbers" - w3schools
+    Available at: https://www.w3schools.com/python/python_numbers.asp [Accessed: 29/01/21]
+[3] "7.1. Fancier Output Formatting" - The Python Standard Library 
+    Available at: https://docs.python.org/3/tutorial/inputoutput.html [Accessed: 29/01/21]
+'''
