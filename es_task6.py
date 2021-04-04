5b460894440a8fa1db4594b98848e9ede456ad04
Izardo <isabelladoylegti@gmail.com>
2021-04-04 20:49:14 +0100

    corrected minor code bug

--- es_task6.py
+++ es_task6.py
@@ -7,7 +7,14 @@ def readText(fileName):                 # creates a function with one parameter 
 
     with open(fileName, 'rt') as f:     # opens the file in read-text mode and assigns it the alias "f"
         read = f.read()                 # reads in the file and assigns it to the value read
-        count = readTxt.count("e")      # count method returns no. of instances the character "e" appears in the file
+        count = read.count("e")         # count method returns no. of instances the character "e" appears in the file
         print(count)                    # prints the count when the function is called
 
-readText("moby-dick.txt")               # calls the readText function with the file name input as an argument
+readText("moby-dick-task6.txt")         # calls the readText function with the file name input as an argument
+
+'''
+REFERENCES:
+
+[1] "Reading and Writing Files in Python (Guide)" - James Mertz <br/>
+Available at: https://realpython.com/read-write-files-python/ [Accessed: 15/3/2021]
+'''
