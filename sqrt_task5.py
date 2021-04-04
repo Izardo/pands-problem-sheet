8124a8758ce3fdd921bc2633d3dfec686996f680
Izardo <isabelladoylegti@gmail.com>
2021-04-04 20:43:46 +0100

    minor code change in for loop, added refs

--- sqrt_task5.py
+++ sqrt_task5.py
@@ -8,7 +8,7 @@ def sqrt(number, iterations = 10):
 
     a = float(number)           # value that we want to get the square root of
 
-    for i in range(iterations): # this for loop iterates 10 times get an approximate sqaure root of a number
+    for _ in range(iterations): # this for loop iterates 10 times get an approximate sqaure root of a number
         number = 0.5 * (number + a / number)  # uses newton's formula to estimate the square of a number
     return round(number, 1)     # rounds number to 1 decimal place
 
@@ -21,3 +21,13 @@ if number <= 0:
     print("Please enter a POSITIVE number: ")  # prompts the user to enter a positive number
 else:  
     print("The square root of {} is approximately {}".format(number, sqrt(number)))
+
+
+'''
+REFERENCES:
+
+[1] "Find root of a number using Newton's method" - GeeksforGeeks <br/>
+Available at: https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/#:~:text=Let%20N%20be%20any%20number,correct%20square%20root%20of%20N 
+[2] "Newton Square Root Method in Python" - Siddik Acil <br/>
+Available at: https://medium.com/@sddkal/newton-square-root-method-in-python-270853e9185d
+'''
