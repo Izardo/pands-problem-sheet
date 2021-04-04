e0edcf019291f8f8d63bd988e712030e36e25a84
Izardo <isabelladoylegti@gmail.com>
2021-04-04 19:20:51 +0100

    Added references, edited comment

--- calculateBMI_task1.py
+++ calculateBMI_task1.py
@@ -1,5 +1,5 @@
-# This program asks the user to input their height 
-# and weight and calculates their BMI
+# This program asks the user to input their height and weight
+# weight and calculates their BMI (Body Mass Index)
 
 # Author: Isabella Doyle
 
@@ -12,3 +12,13 @@ BMI = str(round(weight/((height/100)**2),2))    # assigns the formula for calcul
                                                 # in the print statement
 
 print("Your BMI is " + BMI)                     # prints the users BMI
+
+
+'''
+REFERENCES:
+[1] "How is BMI calculated?" - Ramsey Health Care, 
+Available at: https://www.ramsayhealth.co.uk/weight-loss-surgery/bmi/bmi-formula [Accessed: 28/1/21]
+[2] Vanderplas, Jacob T. A Whirlwind Tour of Python. O'Reilly Media, 2016. pp. 9
+[3] "BMI Calculator" - WebDoctor, Available at: https://www.bmicalculator.ie/ [Accessed:
+28/1/21]
+'''
