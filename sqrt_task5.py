cb4603bd2d2081a6ce9939ec8b47bc83e0ee90e2
Izardo <isabelladoylegti@gmail.com>
2021-04-14 21:48:43 +0100

    improved code by replacing for loop w/ while loop

--- sqrt_task5.py
+++ sqrt_task5.py
@@ -3,24 +3,27 @@
 
 # Author: Isabella Doyle
 
+# numList stores the result of the for loop iterations from the sqrt function
+numList = [0, 0]   
 # the sqrt function takes in two values, the second is pre-defined
-def sqrt(number, iterations = 10): 
+def sqrt(number): 
 
-    a = float(number)           # value that we want to get the square root of
+    a = float(number)                           # value that we want to get the square root of
 
-    for _ in range(iterations): # this for loop iterates 10 times get an approximate sqaure root of a number
-        number = 0.5 * (number + a / number)  # uses newton's formula to estimate the square of a number
-    return round(number, 1)     # rounds number to 1 decimal place
+    while number != numList[-2]:                # the while loop is executed until the result of the code in the next line is the same twice
+        number = 0.5 * (number + a / number)    # uses newton's formula to estimate the square of a number
+        numList.append(number)                  # number placed in list so it can be compared to the current number breaking the while loop broken when appropriate
+    return round(number, 1)                     # rounds number to 1 decimal place
 
 # main program requests the user to input a positive number
-number = float(input("Please enter a positive number: ")) 
+number = float(input("Please enter a positive number: ")) # requests input from the user, converts input to float and assigns to variable 'number'
 
 # If statment filters out negative numbers and the number 0
 # If else statement executes, the square root is returned
 if number <= 0:  
     print("Please enter a POSITIVE number: ")  # prompts the user to enter a positive number
 else:  
-    print("The square root of {} is approximately {}".format(number, sqrt(number)))
+    print("The square root of {} is approximately {}".format(number, sqrt(number))) # prints the approx. square root of 'number' in a sentence
 
 
 '''
