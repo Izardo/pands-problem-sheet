64011f0b576f09b88056df126a6112fc10363bfd
Izardo <isabelladoylegti@gmail.com>
2021-04-17 21:39:43 +0100

    lessened the amount of conditions in if statement

--- weekdayWeekend_task4.py
+++ weekdayWeekend_task4.py
@@ -4,17 +4,17 @@
 # Author: Isabella Doyle
 
 from datetime import datetime           # imports the datetime module from the datetime library
-today = datetime.today().strftime('%A') # accesses the current day with the today() method, while the .strftime() 
-                                        # method converts the current day to a string value and assigns it to the variable 'today'
+today = datetime.today().strftime("%A") # accesses the current day with the today() method. The .strftime() 
+                                        # method converts the current day to a string value. It is assigned 
+                                        # it to the variable 'today'
 
 # if-else statement determines which print statement will be executed, 
 # if the value in variable 'today' matches a weekday, the first statement will be executed 
 # otherwise the second statment will be executed
-if today == "Monday" or "Tuesday" or "Wednesday" or "Thursday":
-    print("Today is {}. It's a weekday!".format(today))
+if today != "Saturday" or today != "Sunday":
+    print("Today is {}. It is a weekday.".format(today))
 else:
-    print("Today is {}. It is the weekend. Yay!")
-
+    print("Today is {}. It is the weekend. Yay!".format(today))
 
 '''
 REFERENCES:
